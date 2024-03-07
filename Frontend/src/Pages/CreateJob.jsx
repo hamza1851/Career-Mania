651d54068ad2c029222b67425a396f2f7a788e15
hamza1851 <hamza.dev011@gmail.com>
2024-03-07 18:16:30 +0530

    partially completed frontend

---
+++ Frontend/src/Pages/CreateJob.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CreateJob() {
+  return (
+    <div className="max-w-screen-2xl container mx-auto xl:px-24 px-4">
+      <div className="bg-[#FAFAFA] py-10px-4 lg:px-16"></div>
+    </div>
+  );
+}
+
+export default CreateJob;
--- Frontend/src/Router/Router.jsx
+++ Frontend/src/Router/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../Pages/Home";
 import About from "../Pages/About";
+import CreateJob from "../Pages/CreateJob";
 
 const Router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ const Router = createBrowserRouter([
     children: [
       { path: "/", element: <Home /> },
       { path: "/about", element: <About /> },
+      {
+        path: "/post-job",
+        element: <CreateJob />,
+      },
     ],
   },
 ]);
